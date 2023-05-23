24a35422b99e61583aa76fd53e859b66210b28d9
Alan Shaw <alan.shaw@protocol.ai>
2023-05-23 16:07:37 +0100

    fix: em ups

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-/**
- * Usage:
- *   POST /bafyDAGRootCID/bagyCARCID
- * (Dudewhere format)
- */
 import { CID } from 'multiformats/cid'
 import { encode } from '@ipld/dag-json'
 import { CarIndexer } from '@ipld/car/indexer'
@@ -26,82 +21,100 @@ export default {
    * @param {unknown} ctx
    */
   async fetch (request, env, ctx) {
-    if (request.method !== 'POST') {
-      return new ErrorResponse('method not allowed', 405)
-    }
-
-    const reqURL = new URL(request.url)
-    /** @type {import('multiformats').UnknownLink} */
-    let rootCID
     try {
-      rootCID = CID.parse(reqURL.pathname.split('/')[1])
-    } catch (err) {
-      return new ErrorResponse('invalid CID', 400)
-    }
+      if (request.method !== 'POST') {
+        return new ErrorResponse('method not allowed', 405)
+      }
 
-    /** @type {import('multiformats').Link} */
-    let carCID
-    try {
-      carCID = CID.parse(reqURL.pathname.split('/')[2])
-    } catch (err) {
-      return new ErrorResponse('invalid CAR CID', 400)
-    }
-    if (carCID.code !== CAR_CODEC) {
-      return new ErrorResponse(`not a CAR CID: ${carCID}`, 400)
-    }
+      const reqURL = new URL(request.url)
+      /** @type {import('multiformats').UnknownLink} */
+      let rootCID
+      try {
+        rootCID = CID.parse(reqURL.pathname.split('/')[1])
+      } catch (err) {
+        return new ErrorResponse('invalid CID', 400)
+      }
 
-    const carParkKey = `${carCID}/${carCID}.car`
-    const carMeta = await env.CARPARK.head(carParkKey)
-    if (!carMeta) {
-      return new ErrorResponse('CAR not found', 404)
-    }
+      /** @type {import('multiformats').Link} */
+      let carCID
+      try {
+        carCID = CID.parse(reqURL.pathname.split('/')[2])
+      } catch (err) {
+        return new ErrorResponse('invalid CAR CID', 400)
+      }
+      if (carCID.code !== CAR_CODEC) {
+        return new ErrorResponse(`not a CAR CID: ${carCID}`, 400)
+      }
 
-    const satNavKey = `${carCID}/${carCID}.car.idx`
-    let satNavMeta = await env.SATNAV.head(satNavKey)
-    if (!satNavMeta) {
-      const res = await env.CARPARK.get(carParkKey)
-      if (!res) {
+      const carParkKey = `${carCID}/${carCID}.car`
+      const carMeta = await env.CARPARK.head(carParkKey)
+      if (!carMeta) {
         return new ErrorResponse('CAR not found', 404)
       }
 
-      const indexer = await CarIndexer.fromIterable(res.body)
-      const { writer, out } = MultihashIndexSortedWriter.create()
-
-      ;(async () => {
-        for await (const blockIndexData of indexer) {
-          await writer.put(blockIndexData)
-        }
-        await writer.close()
-      })()
-
-      const chunks = []
-      for await (const chunk of out) {
-        chunks.push(chunk)
-      }
-
-      // @ts-expect-error
-      await env.SATNAV.put(satNavKey, new Blob(chunks))
-
-      satNavMeta = await env.SATNAV.head(satNavKey)
+      const satNavKey = `${carCID}/${carCID}.car.idx`
+      let satNavMeta = await env.SATNAV.head(satNavKey)
       if (!satNavMeta) {
-        return new ErrorResponse('index not found after creation', 500)
+        const res = await env.CARPARK.get(carParkKey)
+        if (!res) {
+          return new ErrorResponse('CAR not found', 404)
+        }
+
+        const indexer = await CarIndexer.fromIterable(res.body)
+        // const indexer = await CarIndexer.fromIterable((async function * () {
+        //   const reader = res.body.getReader()
+
+        //   try {
+        //     while (true) {
+        //       const { done, value } = await reader.read()
+        //       if (done) return
+        //       yield value
+        //     }
+        //   } finally {
+        //     reader.releaseLock()
+        //   }
+        // })())
+        const { writer, out } = MultihashIndexSortedWriter.create()
+
+        ;(async () => {
+          for await (const blockIndexData of indexer) {
+            await writer.put(blockIndexData)
+          }
+          await writer.close()
+        })()
+
+        const chunks = []
+        for await (const chunk of out) {
+          chunks.push(chunk)
+        }
+
+        // @ts-expect-error
+        await env.SATNAV.put(satNavKey, new Blob(chunks).stream())
+
+        satNavMeta = await env.SATNAV.head(satNavKey)
+        if (!satNavMeta) {
+          return new ErrorResponse('index not found after creation', 500)
+        }
       }
-    }
 
-    const carKeys = await listAll(env.DUDEWHERE, `${rootCID}/`)
-    const dudeWhereKey = `${rootCID}/${carCID}`
-    if (!carKeys.includes(dudeWhereKey)) {
-      await env.DUDEWHERE.put(dudeWhereKey, new Uint8Array())
-      carKeys.push(dudeWhereKey)
-    }
+      const carKeys = await listAll(env.DUDEWHERE, `${rootCID}/`)
+      const dudeWhereKey = `${rootCID}/${carCID}`
+      if (!carKeys.includes(dudeWhereKey)) {
+        await env.DUDEWHERE.put(dudeWhereKey, new Uint8Array())
+        carKeys.push(dudeWhereKey)
+      }
 
-    return new Response(encode({
-      root: rootCID,
-      car: carCID,
-      car_size: carMeta.size,
-      index_size: satNavMeta.size,
-      origin_cars: carKeys.map(k => k.replace(`${rootCID}/`, '')).map(cid => CID.parse(cid))
-    }), { headers: jsonContent })
+      return new Response(encode({
+        root: rootCID,
+        car: carCID,
+        car_size: carMeta.size,
+        index_size: satNavMeta.size,
+        origin_cars: carKeys.map(k => k.replace(`${rootCID}/`, '')).map(cid => CID.parse(cid))
+      }), { headers: jsonContent })
+    } catch (err) {
+      console.error(err)
+      return new ErrorResponse(err.message, 500)
+    }
   }
 }
 
